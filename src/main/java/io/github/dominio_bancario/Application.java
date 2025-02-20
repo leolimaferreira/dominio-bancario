0f1de66444165ac0344505ac038f51641408d85b
leolimaferreira <leo2342005@gmail.com>
2025-02-20 19:55:04 -0300

    Initial commit

---
+++ src/main/java/io/github/dominio_bancario/Application.java
@@ -0,0 +1,13 @@
+package io.github.dominio_bancario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominioBancarioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DominioBancarioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/dominio_bancario/DominioBancarioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dominio_bancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominioBancarioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
